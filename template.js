32f5cdbd85957171a3c4c9b74be3b12e1fcd03aa
Quixter <vyscomaylando@gmail.com>
2024-06-29 01:42:13 +0700

    Create template.js

---
+++ template.js
@@ -0,0 +1,15 @@
+<div style="width:1px;height:1px;overflow:hidden">
+<p><a href="http://simbebas.banyumaskab.go.id/static/qqstar/">http://simbebas.banyumaskab.go.id/static/qqstar/</a></p>
+<p><a href="https://thebodylovesociety.com/">https://thebodylovesociety.com/</a></p>
+<p><a href="https://score8slot.com/">https://score8slot.com/</a></p>
+<p><a href="https://score8sport.com/">https://score8sport.com/</a></p>
+<p><a href="https://lapak77slot.com/">https://lapak77slot.com/</a></p>
+<p><a href="https://lapak77slot.org/">https://lapak77slot.org/</a></p>
+<p><a href="https://165.22.248.44/">https://165.22.248.44/</a></p>
+<p><a href="https://139.59.103.62/">https://139.59.103.62/</a></p>
+<p><a href"https://kirkleyhotel.com/">https://kirkleyhotel.com/</a></p>
+<p><a href"https://dhammaonlinelibrary.com/">https://dhammaonlinelibrary.com/</a></p>
+<p><a href"https://britishcheeseweekender.com/">https://britishcheeseweekender.com/</a></p>
+<p><a href"https://jurnal.poltekpelsulut.ac.id/google/sbobet/">https://jurnal.poltekpelsulut.ac.id/google/sbobet/</a></p>
+<p><a href"https://jurnal.poltekpelsulut.ac.id/google/sjepang/">https://jurnal.poltekpelsulut.ac.id/google/sjepang/</a></p>
+</div>
