03c96c6805027fd5b641d136f8e8d15f5704d271
Quixter <vyscomaylando@gmail.com>
2024-07-16 00:16:17 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,40 +1,5 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://score8slot.com/">https://score8slot.com/</a></p>
-<p><a href="https://score8sport.com/">https://score8sport.com/</a></p>
-<p><a href="https://lapak77slot.com/">https://lapak77slot.com/</a></p>
-<p><a href="https://lapak77slot.org/">https://lapak77slot.org/</a></p>
-<p><a href="https://165.22.248.44/">https://165.22.248.44/</a></p>
-<p><a href="https://139.59.103.62/">https://139.59.103.62/</a></p>
-<p><a href="https://www.kirkleyhotel.com/">https://www.kirkleyhotel.com/</a></p>
-<p><a href="https://www.dhammaonlinelibrary.com/">https://www.dhammaonlinelibrary.com/</a></p>
-<p><a href="https://britishcheeseweekender.com/">https://britishcheeseweekender.com/</a></p>
-<p><a href="https://www.bodybymilk.com/">https://www.bodybymilk.com/</a></p>
-<p><a href="https://loicwacquant.net/">https://loicwacquant.net/</a></p>
-<p><a href="https://www.amazingchinesecuisine.com/">https://www.amazingchinesecuisine.com/</a></p>
-<p><a href="https://www.askives.com/">https://www.askives.com/</a></p>
-<p><a href="https://colombianbrides.net/">https://colombianbrides.net/</a></p>
-<p><a href="https://escorpionsas.com/">https://escorpionsas.com/</a></p>
-<p><a href="https://www.healthystores.org/">https://www.healthystores.org/</a></p>
-<p><a href="https://healthysuccessreviews.com/">https://healthysuccessreviews.com/</a></p>
-<p><a href="https://ketobhbpills.org/">https://ketobhbpills.org/</a></p>
-<p><a href="https://meetupislamabad.com/">https://meetupislamabad.com/</a></p>
-<p><a href="https://www.nesdcelticfaire.com/">https://www.nesdcelticfaire.com/</a></p>
-<p><a href="https://www.progettopo.net/">https://www.progettopo.net/</a></p>
-<p><a href="https://www.score8.bio/">https://www.score8.bio/</a></p>
-<p><a href="https://www.score8.co.com/">https://www.score8.co.com/</a></p>
-<p><a href="https://score8slot.org/">https://score8slot.org/</a></p>
-<p><a href="https://score8sport.id/">https://score8sport.id/</a></p>
-<p><a href="https://subwaycrush.net/">https://subwaycrush.net/</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/bht/">slot gacor</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/gsc/">slot gacor</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/cst/">slot gacor</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/dewaspin777/">dewaspin777</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/kawahtoto/">kawahtoto</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/lapak77/">lapak77</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/pakar77/">pakar77</a></p>
-<p><a href="https://adminseleksijpt.tangerangselatankota.go.id/public/sunda787/">sunda787</a></p>
-<p><a href="https://e-learning.stpreinha.ac.id/filter/google/?google=interwin slot pragmatic">slot gacor</a></p>
-<p><a href="https://e-learning.stpreinha.ac.id/filter/google/?google=kamartoto">kamartoto</a></p>
-<p><a href="https://e-learning.stpreinha.ac.id/filter/google/?google=kawahtoto">kawahtoto</a></p>
+<p><a href="</a></p>
+
 </div>
 
