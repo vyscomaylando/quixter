b30c6d3c266793e1164f91fa76005a427f1a9873
Quixter <vyscomaylando@gmail.com>
2024-07-21 16:35:54 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,5 +1,6 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/score8/">https://simpelp3dn.tangerangselatankota.go.id/public/score8/</a></p>
+<p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/">https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/</a></p>
+<p><a href="https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/">https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/</a></p>
 
 </div>
 
