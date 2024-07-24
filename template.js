1f9bf0908f5f0bbe39452d7c1f51c9986336bc77
Quixter <vyscomaylando@gmail.com>
2024-07-24 20:29:26 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,6 +1,14 @@
 <div style="width:1px;height:1px;overflow:hidden">
 <p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/">https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/</a></p>
 <p><a href="https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/">https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/</a></p>
-
+<p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/">https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/</a></p>
+<p><a href="https://sisumakerpublik.tangerangselatankota.go.id/public/score8/">https://sisumakerpublik.tangerangselatankota.go.id/public/score8/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/score8/">http://simbebas.banyumaskab.go.id/score8/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/interwin/">http://simbebas.banyumaskab.go.id/interwin/</a></p>
+<p><a href="https://inisa.ac.id/score8/">https://inisa.ac.id/score8/</a></p>
+<p><a href="https://inisa.ac.id/sgacor/">https://inisa.ac.id/sgacor/</a></p>
+<p><a href="https://www.pa-sintang.go.id/wibu69/">https://www.pa-sintang.go.id/wibu69/</a></p>
+<p><a href="https://jurnal.stpreinha.ac.id/wibu69/">https://jurnal.stpreinha.ac.id/wibu69/</a></p>
+<p><a href="https://stpreinha.ac.id/lib/wibu69/">https://stpreinha.ac.id/lib/wibu69/</a></p>
 </div>
 
