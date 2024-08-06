6a90b2dbba99c356b770789828ffdde77fe61045
Quixter <vyscomaylando@gmail.com>
2024-08-06 13:56:45 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,14 +1,18 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/">https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/</a></p>
-<p><a href="https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/">https://sisumakerpublik.tangerangselatankota.go.id/public/interwin/</a></p>
-<p><a href="https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/">https://simpelp3dn.tangerangselatankota.go.id/public/slotgacor/</a></p>
-<p><a href="https://sisumakerpublik.tangerangselatankota.go.id/public/score8/">https://sisumakerpublik.tangerangselatankota.go.id/public/score8/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/score8/">http://simbebas.banyumaskab.go.id/score8/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/interwin/">http://simbebas.banyumaskab.go.id/interwin/</a></p>
-<p><a href="https://inisa.ac.id/score8/">https://inisa.ac.id/score8/</a></p>
-<p><a href="https://inisa.ac.id/sgacor/">https://inisa.ac.id/sgacor/</a></p>
-<p><a href="https://www.pa-sintang.go.id/wibu69/">https://www.pa-sintang.go.id/wibu69/</a></p>
-<p><a href="https://jurnal.stpreinha.ac.id/wibu69/">https://jurnal.stpreinha.ac.id/wibu69/</a></p>
-<p><a href="https://stpreinha.ac.id/lib/wibu69/">https://stpreinha.ac.id/lib/wibu69/</a></p>
+<p><a href="https://lawyerinjeddah.com/">https://lawyerinjeddah.com/</a></p>
+<p><a href="https://suanhaducphuc.com/">https://suanhaducphuc.com/</a></p>
+<p><a href="https://tuthudey.com/">https://tuthudey.com/</a></p>
+<p><a href="https://inmanscchamber.org/">https://inmanscchamber.org/</a></p>
+<p><a href="https://flatata.com/">https://flatata.com/</a></p>
+<p><a href="https://zeitzuteilen.blog/">https://zeitzuteilen.blog/</a></p>
+<p><a href="https://www.nesdcelticfaire.com/">https://www.nesdcelticfaire.com/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/betogel/">http://simbebas.banyumaskab.go.id/static/vendor/betogel/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/hometogel/">http://simbebas.banyumaskab.go.id/static/vendor/hometogel/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/jonitogel/">http://simbebas.banyumaskab.go.id/static/vendor/jonitogel/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/kamartoto/">http://simbebas.banyumaskab.go.id/static/vendor/kamartoto/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/beton888/">http://simbebas.banyumaskab.go.id/static/vendor/beton888/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/bandel88/">http://simbebas.banyumaskab.go.id/static/vendor/bandel88/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/zenwin88/">http://simbebas.banyumaskab.go.id/static/vendor/zenwin88/</a></p>
+<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/lapak77/">http://simbebas.banyumaskab.go.id/static/vendor/lapak77/</a></p>
 </div>
 
