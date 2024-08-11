223b5ed4a5fe4d19d2616682dd091976fe99a3da
Quixter <vyscomaylando@gmail.com>
2024-08-11 14:15:46 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,18 +1,7 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://lawyerinjeddah.com/">https://lawyerinjeddah.com/</a></p>
-<p><a href="https://suanhaducphuc.com/">https://suanhaducphuc.com/</a></p>
-<p><a href="https://tuthudey.com/">https://tuthudey.com/</a></p>
-<p><a href="https://inmanscchamber.org/">https://inmanscchamber.org/</a></p>
-<p><a href="https://flatata.com/">https://flatata.com/</a></p>
-<p><a href="https://zeitzuteilen.blog/">https://zeitzuteilen.blog/</a></p>
-<p><a href="https://www.nesdcelticfaire.com/">https://www.nesdcelticfaire.com/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/betogel/">http://simbebas.banyumaskab.go.id/static/vendor/betogel/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/hometogel/">http://simbebas.banyumaskab.go.id/static/vendor/hometogel/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/jonitogel/">http://simbebas.banyumaskab.go.id/static/vendor/jonitogel/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/kamartoto/">http://simbebas.banyumaskab.go.id/static/vendor/kamartoto/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/beton888/">http://simbebas.banyumaskab.go.id/static/vendor/beton888/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/bandel88/">http://simbebas.banyumaskab.go.id/static/vendor/bandel88/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/zenwin88/">http://simbebas.banyumaskab.go.id/static/vendor/zenwin88/</a></p>
-<p><a href="http://simbebas.banyumaskab.go.id/static/vendor/lapak77/">http://simbebas.banyumaskab.go.id/static/vendor/lapak77/</a></p>
+<p><a href="https://www.filmhead.com/">https://www.filmhead.com/</a></p>
+<p><a href="https://referensi.univ-bd.ac.id/vendor/imbagacor/">imbagacor</a></p>
+<p><a href="https://referensi.univ-bd.ac.id/vendor/imbajp/">imbajp</a></p>
+<p><a href="https://referensi.univ-bd.ac.id/vendor/gk8/">gk8</a></p>
 </div>
 
