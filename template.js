38948e4fa51b7134bd5d232758d3b6307192fa19
Quixter <vyscomaylando@gmail.com>
2024-08-17 14:21:26 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,20 +1,23 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://www.filmhead.com/">https://www.filmhead.com/</a></p>
-<p><a href="https://referensi.univ-bd.ac.id/vendor/imbagacor/">imbagacor</a></p>
-<p><a href="https://referensi.univ-bd.ac.id/vendor/imbajp/">imbajp</a></p>
-<p><a href="https://referensi.univ-bd.ac.id/vendor/gk8/">gk8</a></p>
-<p><a href="https://hazladetos.org/">https://hazladetos.org/</a></p>
+<p><a href="https://www.bodybymilk.com/">score8</a></p>
+<p><a href="https://healthystores.org/">score8</a></p>
+<p><a href="https://www.nesdcelticfaire.com/">score8</a></p>
+<p><a href="https://www.nesdcelticfaire.com/">score8</a></p>
 <p><a href="https://colombianbrides.net/">https://colombianbrides.net/</a></p>
-<p><a href="https://fti.itats.ac.id/toko/imbaslot/">imbaslot</a></p>
-</div>
-
-<div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://feb.umk.ac.id/struggle/kamboja/">https://feb.umk.ac.id/struggle/kamboja/</a></p>
-<p><a href="https://feb.umk.ac.id/struggle/hoki/">https://feb.umk.ac.id/struggle/hoki/</a></p>
-<p><a href="https://feb.umk.ac.id/struggle/xthai/">https://feb.umk.ac.id/struggle/xthai/</a></p>
-<p><a href="https://feb.umk.ac.id/app/ziatogel/">https://feb.umk.ac.id/app/ziatogel/</a></p>
-<p><a href="https://feb.umk.ac.id/app/betogel/">https://feb.umk.ac.id/app/betogel/</a></p>
-<p><a href="https://feb.umk.ac.id/app/hometogel/">https://feb.umk.ac.id/app/hometogel/</a></p>
-<p><a href="https://feb.umk.ac.id/app/jonitogel/">https://feb.umk.ac.id/app/jonitogel/</a></p>
-<p><a href="https://feb.umk.ac.id/app/kamartoto/">https://feb.umk.ac.id/app/kamartoto/</a></p>
+<p><a href="https://www.filmhead.com/">https://www.filmhead.com/</a></p>
+<p><a href="https://bumdes.sugihwaras.desa.id/struggle/totoloka88/">https://bumdes.sugihwaras.desa.id/struggle/totoloka88/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/playbook88/">https://ppid.sugihwaras.desa.id/struggle/playbook88/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/aquaslot/">https://ppid.sugihwaras.desa.id/struggle/aquaslot/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/prada188/">https://ppid.sugihwaras.desa.id/struggle/prada188/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/dinobet/">https://ppid.sugihwaras.desa.id/struggle/dinobet/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/berkah303/">https://ppid.sugihwaras.desa.id/struggle/berkah303/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/zenwin88/">https://ppid.sugihwaras.desa.id/struggle/zenwin88/</a></p>
+<p><a href="https://ppid.sugihwaras.desa.id/struggle/bandel88/">https://ppid.sugihwaras.desa.id/struggle/bandel88/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/surgadewa/">https://suksesbersama.sugihwaras.desa.id/struggle/surgadewa/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/dana123/">https://suksesbersama.sugihwaras.desa.id/struggle/dana123/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/rajaindo/">https://suksesbersama.sugihwaras.desa.id/struggle/rajaindo/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/lapak77/">https://suksesbersama.sugihwaras.desa.id/struggle/lapak77/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/imbagacor/">https://suksesbersama.sugihwaras.desa.id/struggle/imbagacor/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/imbaslot/">https://suksesbersama.sugihwaras.desa.id/struggle/imbaslot/</a></p>
+<p><a href="https://suksesbersama.sugihwaras.desa.id/struggle/imbajp/">https://suksesbersama.sugihwaras.desa.id/struggle/imbajp/</a></p>
 </div>
