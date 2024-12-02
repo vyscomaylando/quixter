53b8397ad42c530a5db22dad68ddd36baf205667
2S Struggle Street <vyscomaylando@gmail.com>
2024-12-02 12:32:57 +0700

    Update template.js

--- template.js
+++ template.js
@@ -98,6 +98,7 @@
 <p><a href="https://epustaka.stiesabang.ac.id/thailand/">https://epustaka.stiesabang.ac.id/thailand/</a></p>
 <p><a href="https://epustaka.stiesabang.ac.id/gacor/">https://epustaka.stiesabang.ac.id/gacor/</a></p>
 <p><a href="https://interwin.lapssjk.id/">https://interwin.lapssjk.id/</a></p>
+<p><a href="https://slot200.lapssjk.id/">https://slot200.lapssjk.id/</a></p>
 <p><a href="https://prokopim.setda.langsakota.go.id/app/sthai/">https://prokopim.setda.langsakota.go.id/app/sthai/</a></p>
 <p><a href="https://prokopim.setda.langsakota.go.id/app/shoki/">https://prokopim.setda.langsakota.go.id/app/shoki/</a></p>
 <p><a href="https://prokopim.setda.langsakota.go.id/public/system/">https://prokopim.setda.langsakota.go.id/public/system/</a></p>
