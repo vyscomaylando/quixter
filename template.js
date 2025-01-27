76e64afc0ab1e29b5b698c3bb204f54bfe5d58d2
2S Struggle Street <vyscomaylando@gmail.com>
2025-01-27 12:52:58 +0700

    Update template.js

--- template.js
+++ template.js
@@ -52,4 +52,5 @@
 <p><a href="https://joker88s.glitch.me/">https://joker88s.glitch.me/</a><p>
 <p><a href="https://jok88.glitch.me/">joker88 link alternatif</a><p>
 <P><a href="http://joker88-5000.com/">Joker88</a></p>
+<P><a href="https://joker88-10k.com/">https://joker88-10k.com/</a></p>
 </div>
