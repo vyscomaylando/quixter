9deebd9a4ae07f46a1e3e67aa37149fdd23e15a1
2S Struggle Street <vyscomaylando@gmail.com>
2025-02-08 15:15:13 +0700

    Update template.js

--- template.js
+++ template.js
@@ -41,5 +41,6 @@
 <p><a href="https://joker88-5000.com/jokerslot/">joker88 slot</a></p>
 <p><a href="https://joker88-5000.com/slot/">joker88</a></p>
 <p><a href="https://joker88-5000.com/pusat/">Joker88</a></p>
-<p><a href="https://www.bridgecommanderfiles.com/">https://www.bridgecommanderfiles.com/</a></p>
+<p><a href="https://www.bridgecommanderfiles.com/">Joker88</a></p>
+<p><a href="https://pmb.akabi.ac.id/">Joker88</a></p>
 </div>
