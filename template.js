97bd0e905b076b1fb829e6bb43b0f59d8428d963
2S Struggle Street <vyscomaylando@gmail.com>
2025-02-08 18:02:44 +0700

    Update template.js

--- template.js
+++ template.js
@@ -50,4 +50,5 @@
 <p><a href="https://joker88-10k.com/maxwin/">https://joker88-10k.com/maxwin/</a></p>
 <p><a href="https://joker88-10k.com/toto/">https://joker88-10k.com/toto/</a></p>
 <p><a href="https://www.lighthouseshoppe.com/">https://www.lighthouseshoppe.com/</a></p>
+<p><a href="https://www.sportsinfocentral.com/">https://www.sportsinfocentral.com/</a></p>
 </div>
