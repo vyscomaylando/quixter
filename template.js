421eaa6ff6f9724330dc4abc96256a7aed0b9050
2S Struggle Street <vyscomaylando@gmail.com>
2025-02-26 00:45:53 +0700

    Update template.js

--- template.js
+++ template.js
@@ -66,4 +66,5 @@
 <p><a href="https://justdohits.com/">slot maxwin</a></p>
 <p><a href="https://kkn.umj.ac.id/upload/gallery/">https://kkn.umj.ac.id/upload/gallery/</a></p>
 <p><a href="https://kkn.umj.ac.id/upload/gallery/dana/">https://kkn.umj.ac.id/upload/gallery/dana/</a></p>
+<p><a href="https://kkn.umj.ac.id/upload/gallery/5000/">https://kkn.umj.ac.id/upload/gallery/5000/</a></p>
 </div>
