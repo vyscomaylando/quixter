67a2b0c26ea84502a13f0574d764440f5a79b2e1
2S Struggle Street <vyscomaylando@gmail.com>
2025-02-26 22:23:46 +0700

    Update template.js

--- template.js
+++ template.js
@@ -80,4 +80,5 @@
 <p><a href="https://sipp.ptun-surabaya.go.id/jaksa/">Slot Gacor</a></p>
 <p><a href="https://sipp.ptun-surabaya.go.id/scatter/">https://sipp.ptun-surabaya.go.id/scatter/</a></p>
 <p><a href="https://sipp.ptun-surabaya.go.id/gac/">https://sipp.ptun-surabaya.go.id/gac/</a></p>
+<p><a href="https://sipp.ptun-surabaya.go.id/pengadilan/">Slot Maxwin Hari Ini</a></p>
 </div>
