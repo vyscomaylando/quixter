21ddbbd038cee614bc25a21184a71be0dc4754f6
2S Struggle Street <vyscomaylando@gmail.com>
2025-03-04 13:28:12 +0700

    Update template.js

--- template.js
+++ template.js
@@ -114,4 +114,5 @@
 <p><a href="https://lpm.uinsgd.ac.id/wp-content/uploads/2021/02/bkp/toto/">https://lpm.uinsgd.ac.id/wp-content/uploads/2021/02/bkp/toto/</a></p>
 <p><a href="https://lpm.uinsgd.ac.id/wp-content/uploads/2021/02/viral/">https://lpm.uinsgd.ac.id/wp-content/uploads/2021/02/viral/</a></p>
 <p><a href="https://lpm.uinsgd.ac.id/sunan/">https://lpm.uinsgd.ac.id/sunan/</a></p>
+<p><a href="https://journal.dkpp.go.id/run/">slot gacor</a></p>
 </div>
