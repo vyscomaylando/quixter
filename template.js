ca3a0c94c39300359363f81c721148f1a041a79e
2S Struggle Street <vyscomaylando@gmail.com>
2025-08-19 15:58:52 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,14 +1,3 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/nelayan-bangka-jackpot-mahjong-ways-pgsoft.html">nelayan bangka jackpot mahjong ways pgsoft</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/buruh-serabutan-trik-scatter-emas-slot.html">buruh serabutan trik scatter emas slot</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/tukang-parkir-bekasi-menang-mahjong-wins-3.html">tukang parkir bekasi menang mahjong wins 3</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/remaja-karawang-maxwin-scatter-hitam.html">remaja karawang maxwin scatter hitam</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/ibu-rumah-tangga-freespin-mahjong-ways-2.html">ibu rumah tangga freespin mahjong ways 2</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/penjual-gorengan-kaya-berkat-slot-pgsoft.html">penjual gorengan kaya berkat slot pgsoft</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/mahasiswa-akhir-jam-gacor-slot-lintas808.html">mahasiswa akhir jam gacor slot lintas808</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/kuli-bangunan-menang-jutaan-mahjong-wins.html">kuli bangunan menang jutaan mahjong wins</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/bapak-rumahan-spin-game-dapat-190-juta.html">bapak rumahan spin game dapat 190 juta</a></p>
-<p><a href="https://union-sg-arfga3f5dcg6apdj.uksouth-01.azurewebsites.net/bocah-medan-hp-kentang-cuan-slot-pgsoft.html">bocah medan hp kentang cuan slot pgsoft</a></p>
-<p><a href="https://journal.uinsgd.ac.id/">slot gacor</a></p>
-
+<p><a href="https://dinkes.langsakota.go.id/">slot gacor</a></p>
 </div>
