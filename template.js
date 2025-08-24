c4d75bc3c234b8a5e02cb065e1b4048439632321
2S Struggle Street <vyscomaylando@gmail.com>
2025-08-24 19:58:43 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,4 +1,5 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://dinkes.langsakota.go.id/">slot gacor</a></p>
-<p><a href="https://bkpsdm.langsakota.go.id/">slot gacor 777</a></p>
+<p><a href="https://bkpsdm.langsakota.go.id/simpeg/data/">https://bkpsdm.langsakota.go.id/simpeg/data/</a></p>
+<p><a href="https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/">https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/</a></p>
+<p><a href="https://www.staisni.ac.id/gelar-akademik/">https://www.staisni.ac.id/gelar-akademik/</a></p>
 </div>
