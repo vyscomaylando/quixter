88c7b98448d1578076a7b30198ccf32a0baf5c56
2S Struggle Street <vyscomaylando@gmail.com>
2025-08-27 05:53:54 +0700

    Update template.js

--- template.js
+++ template.js
@@ -3,4 +3,5 @@
 <p><a href="https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/">https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/</a></p>
 <p><a href="https://www.staisni.ac.id/gelar-akademik/">https://www.staisni.ac.id/gelar-akademik/</a></p>
 <p><a href="https://lintas808h.com/">lintas808</a></p>
+<p><a href="https://elearning.staisni.ac.id/">LINTAS808</a></p>
 </div>
