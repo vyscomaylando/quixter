cc47f34e9056ed98ab83987af06be1affbd5f1e8
2S Struggle Street <vyscomaylando@gmail.com>
2025-09-09 03:33:19 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,17 +1,5 @@
 <div style="width:1px;height:1px;overflow:hidden">
 <p><a href="https://lintas808.com/">https://lintas808.com/</a></p>
 <p><a href="https://lintas808.biz/">https://lintas808.biz/</a></p>
-<p><a href="https://bkpsdm.langsakota.go.id/simpeg/data/">https://bkpsdm.langsakota.go.id/simpeg/data/</a></p>
-<p><a href="https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/">https://bkpsdm.langsakota.go.id/pages/kata-sambutan/sgacor/</a></p>
-<p><a href="https://www.staisni.ac.id/gelar-akademik/">https://www.staisni.ac.id/gelar-akademik/</a></p>
 <p><a href="https://lintas808h.com/">lintas808</a></p>
-<p><a href="https://elearning.staisni.ac.id/">LINTAS808</a></p>
-<p><a href="https://pasca.iainlhokseumawe.ac.id/">LINTAS808</a></p>
-<p><a href="https://efin.sdtexmacosemarang.sch.id/">LINTAS808</a></p>
-<p><a href="https://sman66-jkt.sch.id/">LINTAS808</a></p>
-<p><a href="https://journal.staisni.ac.id/">lintas808</a></p>
-<p><a href="https://bkpsdm.langsakota.go.id/">slot gacor</a></p>
-<p><a href="https://sapa.uinsgd.ac.id/">lintas808</a></p>
-<p><a href="https://simonas.langsakota.go.id/">lintas808</a></p>
-<p><a href="https://library.unida.gontor.ac.id/">https://library.unida.gontor.ac.id/</a></p>
 </div>
