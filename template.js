d436ce41355d4a722d4681379012a57cb77f3d6e
2S Struggle Street <vyscomaylando@gmail.com>
2025-09-11 01:02:14 +0700

    Update template.js

--- template.js
+++ template.js
@@ -2,4 +2,5 @@
 <p><a href="https://lintas808.com/">https://lintas808.com/</a></p>
 <p><a href="https://lintas808.biz/">https://lintas808.biz/</a></p>
 <p><a href="https://lintas808h.com/">lintas808</a></p>
+<p><a href="https://skb.salatiga.go.id/">https://skb.salatiga.go.id/</a></p>
 </div>
