1b6732f2ac202276f7682bcaddfb716401ba6122
2S Struggle Street <vyscomaylando@gmail.com>
2025-09-23 23:08:49 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,5 +1,4 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://ppdb.yayasanppittexmaco.or.id/">https://ppdb.yayasanppittexmaco.or.id/</a></p>
 <p><a href="https://lintas808.com/">https://lintas808.com/</a></p>
 <p><a href="https://lintas808-slot.com/">https://lintas808-slot.com/</a></p>
 <p><a href="https://lintas808.biz/">https://lintas808.biz/</a></p>
@@ -8,15 +7,10 @@
 <p><a href="https://linklist.bio/lintas808/">https://linklist.bio/lintas808/</a></p>
 <p><a href="https://linklist.bio/lintas808login">https://linklist.bio/lintas808login</a></p>
 <p><a href="https://heylink.me/lintas808/">https://heylink.me/lintas808/</a></p>
-<p><a href="https://www.policeroadstransport.fs.gov.za/wp-content/cache/lintas808/">https://www.policeroadstransport.fs.gov.za/wp-content/cache/lintas808/</a></p>
-<p><a href="https://bkpsdm.langsakota.go.id/">https://bkpsdm.langsakota.go.id/</a></p>
-<p><a href="https://dinkes.langsakota.go.id/">https://dinkes.langsakota.go.id/</a></p>
-<p><a href="https://www.cogta.fs.gov.za/wp-content/wflogs/sv388/">https://www.cogta.fs.gov.za/wp-content/wflogs/sv388/</a></p>
-<p><a href="https://efin.sdtexmacosemarang.sch.id/">https://efin.sdtexmacosemarang.sch.id/</a></p>
-<p><a href="https://sdtexmacosemarang.sch.id/">https://sdtexmacosemarang.sch.id/</a></p>
-<p><a href="https://lppm.umaha.ac.id/">https://lppm.umaha.ac.id/</a></p>
 <p><a href="https://about.me/lintas808">https://about.me/lintas808</a></p>
 <p><a href="http://lintas808gacor.com/">lintas808</a></p>
-<p><a href="https://ppdb.yayasanppittexmaco.or.id/">lintas808</a></p>
-<p><a href="https://www.tacosapson.com/menu">PHISSING</a></p>
+<p><a href="https://www.santaanaemergencyplumbing.com/24-hour-plumber-santa-ana/">https://www.santaanaemergencyplumbing.com/24-hour-plumber-santa-ana/</a></p>
+<p><a href="https://land-sms.com/sms/categories/">https://land-sms.com/sms/categories/</a></p>
+<p><a href="https://memoriesofhalloween.com/">https://memoriesofhalloween.com/</a></p>
+<p><a href="https://buyclenbuterolin.com/">https://buyclenbuterolin.com/</a></p>
 </div>
